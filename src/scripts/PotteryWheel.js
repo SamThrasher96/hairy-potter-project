b338cdf7a01b2bbf179c3d6190ca1585bd9ca4fa
Sam Thrasher <samthrasher9@gmail.com>
2023-05-01 15:23:20 -0500

    Made some pottery, very exciting

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,11 @@
+let id = 1
+
+export const makePottery = (shape, weight, height) => {
+    const pottery = {
+        shape,
+        weight,
+        height,
+        id: id++
+    };
+    return pottery
+}
---
+++ src/scripts/main.js
@@ -0,0 +1,32 @@
+// Imports go first
+
+import { makePottery } from "./PotteryWheel.js"
+
+
+
+// Make 5 pieces of pottery at the wheel
+let mug = makePottery("mug", 7, 3)
+console.log(mug)
+
+let plate = makePottery("plate", 5, 2)
+console.log(plate)
+
+let vase = makePottery("vase", 8, 10)
+console.log(vase)
+
+let jar  = makePottery("jar", 7, 4)
+console.log(jar)
+
+let cat = makePottery("cat", 12, 12)
+console.log(cat)
+
+// Fire each piece of pottery in the kiln
+
+
+// Determine which ones should be sold, and their price
+
+
+// Invoke the component function that renders the HTML list
+
+
+
